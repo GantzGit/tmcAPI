0949a6dca79d52bdac8013014c237a4e4b0b173f
Kyle Dunne <57367343+GantzGit@users.noreply.github.com>
2024-11-07 17:44:56 -0700

    Update get_tmc_mgmt_clusters.sh
    fix jq errors when gathering API token

--- get_tmc_mgmt_clusters.sh
+++ get_tmc_mgmt_clusters.sh
@@ -2,18 +2,18 @@
 
 list_management_clusters() {
   refresh_token="$VMW_CLOUD_API_TOKEN"
-  url="https://console.tanzu.broadcom.com/csp/gateway/am/api/auth/api-tokens/authorize?refresh_token=$refresh_token"
+  url="https://console.tanzu.broadcom.com/csp/gateway/am/api/auth/api-tokens/authorize"  # Removed ?refresh_token=$refresh_token
 
   # Log start time
   echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting management cluster retrieval"
 
   # Get access token
   echo "$(date '+%Y-%m-%d %H:%M:%S') - Retrieving access token..."
-  access_token=$(curl -s -X POST -H "Content-Type: application/json" "$url" | jq -r '.access_token')
+  access_token=$(curl -s -X POST -H "Accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" --data-urlencode "refresh_token=$refresh_token" "$url" | jq -r '.access_token') 
 
   # Validate access token
   if [ -n "$access_token" ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Access token retrieved and validated."
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Access token retrieved and validated: $access_token"
   else
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Failed to retrieve or validate access token. Check your refresh token."
     return 1 # Exit the function with an error code
@@ -21,7 +21,14 @@ list_management_clusters() {
 
   # Get management clusters
   echo "$(date '+%Y-%m-%d %H:%M:%S') - Retrieving management clusters..."
-  management_clusters=$(curl -s -X GET -H "Authorization: Bearer $access_token" "https://mapbusupport.tmc.cloud.vmware.com/v1alpha1/managementclusters" | jq -r '.managementClusters[].fullname.name' | sort)
+  management_clusters_response=$(curl -s -X GET -H "Authorization: Bearer $access_token" "https://mapbusupport.tmc.cloud.vmware.com/v1alpha1/managementclusters")
+
+  # Check if the response contains "managementClusters"
+  if [[ "$management_clusters_response" == *'"managementClusters":'* ]]; then
+    management_clusters=$(echo "$management_clusters_response" | jq -r '.managementClusters[].fullName.name' | sort)
+  else
+    management_clusters=""  # Set to empty if not found
+  fi
 
   # List management clusters
   if [ -n "$management_clusters" ]; then
